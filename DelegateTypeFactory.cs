b5b6941bc4c0eff5152b6057134b12af83230c59
UnknownX7 <UnknownX7@users.noreply.github.com>
2023-02-10 08:28:51 -0600

    Fix hooks

---
+++ DelegateTypeFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace uDev;
+
+public static class DelegateTypeFactory
+{
+    private static readonly ModuleBuilder module;
+
+    static DelegateTypeFactory()
+    {
+        var assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("DelegateTypeFactory"), AssemblyBuilderAccess.RunAndCollect);
+        module = assembly.DefineDynamicModule("DelegateTypeFactory");
+    }
+
+    public static Type CreateDelegateType(Type[] types)
+    {
+        var builder = module.DefineType(GetUniqueName(), TypeAttributes.Sealed | TypeAttributes.Public, typeof(MulticastDelegate));
+
+        var ctor = builder.DefineConstructor(MethodAttributes.RTSpecialName | MethodAttributes.HideBySig | MethodAttributes.Public,
+            CallingConventions.Standard, new[] { typeof(object), typeof(nint) });
+        ctor.SetImplementationFlags(MethodImplAttributes.CodeTypeMask);
+
+        var paramTypes = types.SkipLast(1).ToArray();
+
+        var invokeMethod = builder.DefineMethod("Invoke", MethodAttributes.HideBySig | MethodAttributes.Virtual | MethodAttributes.Public, types.Last(), paramTypes);
+        invokeMethod.SetImplementationFlags(MethodImplAttributes.CodeTypeMask);
+
+        for (int i = 1; i <= paramTypes.Length; i++)
+            invokeMethod.DefineParameter(i, ParameterAttributes.None, $"a{i}");
+
+        return builder.CreateType();
+    }
+
+    private static string GetUniqueName()
+    {
+        var number = 2;
+        var name = "CustomDelegate";
+        while (module.GetType(name) != null)
+            name = $"CustomDelegate{number++}";
+        return name;
+    }
+}
--- UI/HookUI.cs
+++ UI/HookUI.cs
@@ -54,6 +54,12 @@ public static class HookUI
 
         ImGui.TextUnformatted(")");
 
+        if (ImGui.Button("Create Delegate"))
+        {
+            Hook?.Dispose();
+            hook = CreateHook();
+        }
+        ImGui.SameLine();
         ImGui.Button("Reset");
         if (ImGuiEx.IsItemDoubleClicked())
         {
@@ -62,52 +68,50 @@ public static class HookUI
                 args[i] = 0;
         }
 
-        if (ImGui.Button("Create Delegate"))
-        {
-            Hook?.Dispose();
-
-            var types = args.TakeWhile(id => id != 0).Select(id => typeDictionary[argTypes[id]]).ToList();
-
-            var i = 0;
-            var paramExpressions = types.Select(t => Expression.Parameter(t, $"a{++i}")).ToList();
-
-            var retType = typeDictionary[argTypes[ret]];
-            var hasReturn = retType != typeof(void);
-            if (!hasReturn && types.Count == 0) return;
-
-            types.Add(retType);
-            var hookDelegateType = Expression.GetDelegateType(types.ToArray());
-
-            var hookType = typeof(Hook<>).MakeGenericType(hookDelegateType);
-            var ctor = hookType.GetConstructor(new[] { typeof(nint), hookDelegateType });
-
-            var hookField = Expression.Convert(Expression.Field(null, typeof(HookUI).GetField("hook", BindingFlags.Static | BindingFlags.NonPublic)!), hookType);
-            var getHookOriginal = Expression.Call(hookField, hookType.GetProperty("Original", BindingFlags.Instance | BindingFlags.Public)!.GetMethod!);
-            var callHookOriginal = Expression.Invoke(getHookOriginal, paramExpressions);
-            var retVar = hasReturn ? Expression.Variable(retType, "ret") : null;
-            var assignRet = hasReturn ? Expression.Assign(retVar, callHookOriginal) : null; //Expression.Convert(callHookOriginal, retType)
-
-            var objectArray = paramExpressions.Select(p => Expression.Convert(p, typeof(object)));
-            if (hasReturn)
-                objectArray = objectArray.Append(Expression.Convert(retVar, typeof(object)));
-
-            var concatExpression = Expression.Call(typeof(HookUI).GetMethod("ConcatParams", BindingFlags.Static | BindingFlags.NonPublic)!, Expression.NewArrayInit(typeof(object), objectArray));
-            var printExpression = Expression.Call(typeof(HookUI).GetMethod("LogInfo", BindingFlags.Static | BindingFlags.NonPublic)!, concatExpression);
-
-            var block = hasReturn ? Expression.Block(new[] { retVar }, assignRet, printExpression, retVar) : Expression.Block(printExpression, callHookOriginal);
-            hook = ctor?.Invoke(new object[] { SignatureUI.Address, Expression.Lambda(block, paramExpressions).Compile() });
-        }
-
         if (hook == null) return;
 
         if (ImGui.Button("Enable Hook"))
             hook.GetType().GetMethod("Enable")?.Invoke(hook, null);
-
+        ImGui.SameLine();
         if (ImGui.Button("Dispose Hook") && Hook != null)
         {
             Hook.Dispose();
             hook = null;
         }
+    }
+
+    private static object CreateHook()
+    {
+        var types = args.TakeWhile(id => id != 0).Select(id => typeDictionary[argTypes[id]]).ToList();
+
+        var i = 0;
+        var paramExpressions = types.Select(t => Expression.Parameter(t, $"a{++i}")).ToList();
+
+        var retType = typeDictionary[argTypes[ret]];
+        var hasReturn = retType != typeof(void);
+        if (!hasReturn && types.Count == 0) return null;
+
+        types.Add(retType);
+        var hookDelegateType = DelegateTypeFactory.CreateDelegateType(types.ToArray());
+
+        var hookType = typeof(Hook<>).MakeGenericType(hookDelegateType);
+        var ctor = hookType.GetConstructor(new[] { typeof(nint), hookDelegateType });
+
+        var hookField = Expression.Convert(Expression.Field(null, typeof(HookUI).GetField("hook", BindingFlags.Static | BindingFlags.NonPublic)!), hookType);
+        var getHookOriginal = Expression.Call(hookField, hookType.GetProperty("Original", BindingFlags.Instance | BindingFlags.Public)!.GetMethod!);
+        var callHookOriginal = Expression.Invoke(getHookOriginal, paramExpressions);
+        var retVar = hasReturn ? Expression.Variable(retType, "ret") : null;
+        var assignRet = hasReturn ? Expression.Assign(retVar, callHookOriginal) : null; //Expression.Convert(callHookOriginal, retType)
+
+        var objectArray = paramExpressions.Select(p => Expression.Convert(p, typeof(object)));
+        if (hasReturn)
+            objectArray = objectArray.Append(Expression.Convert(retVar, typeof(object)));
+
+        var concatExpression = Expression.Call(typeof(HookUI).GetMethod("ConcatParams", BindingFlags.Static | BindingFlags.NonPublic)!, Expression.NewArrayInit(typeof(object), objectArray));
+        var printExpression = Expression.Call(typeof(HookUI).GetMethod("LogInfo", BindingFlags.Static | BindingFlags.NonPublic)!, concatExpression);
+
+        var block = hasReturn ? Expression.Block(new[] { retVar }, assignRet, printExpression, retVar) : Expression.Block(printExpression, callHookOriginal);
+        return ctor?.Invoke(new object[] { SignatureUI.Address, Expression.Lambda(hookDelegateType, block, paramExpressions).Compile() });
     }
 
     private static void LogInfo(string message) => PluginLog.Information(message);
@@ -119,10 +123,16 @@ public static class HookUI
         for (int i = 0; i < objects.Length; i++)
         {
             var o = objects[i];
+            var oStr = o switch
+            {
+                nint p => p.ToString("X"),
+                nuint up => up.ToString("X"),
+                _ => o.ToString()
+            };
             if (i != objects.Length - 1)
-                str += $"a{i}: {o} | ";
+                str += $"a{i + 1}: {oStr} | ";
             else
-                str += $"ret: {o}";
+                str += $"ret: {oStr}";
         }
 
         return str;
